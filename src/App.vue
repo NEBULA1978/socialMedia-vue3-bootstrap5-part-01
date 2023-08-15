62accebf8014ccdd2113f9fa0b4dd7f1f246964b
NEBULA1978 <rpascualey@gmail.com>
2023-08-15 23:17:51 +0200

    Video3 red social Vue3

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <NavComp />
+  <router-view/>
+  <TheButton/>
+  
+</template>
+
+<script>
+import NavComp from "@/components/NavComp.vue"
+export default {
+  name: "App",
+  components: {
+    NavComp
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+}
+
+
+</style>
